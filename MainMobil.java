0d221fbda6fb5b787e780785ea9c0ad06970561e
sefinaay <sefinaayudias@gmail.com>
2025-03-09 14:01:30 +0700

    first commit

---
+++ MainMobil.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+//untuk mengambil import pengguna
+
+public class MainMobil {
+    //mendeklarasikan kelas main mobil
+
+    public static void main(String[] args) {
+        //titik awal eksekusi program 
+        
+        //instan objek bernama m1
+        Mobil m1 = new Mobil();
+        //membuat objek m1 dari kelas mobil
+
+        Scanner in = new Scanner (System.in);
+        //untuk membaca input dari pengguna
+
+        System.out.println("Masukan detail mobil 1 : ");
+        //meminta pengguna memasukan detail mobil
+        System.out.print("Manufaktur : ");
+        //meminta pengguna memasukan manufaktur mobil
+        m1.setManufaktur(in.nextLine());
+        //menyimpan data ke objek m1
+        System.out.print("Nomor Plat : ");
+        //meminta pengguna memasukan nomor plat mobil
+        m1.setNoPlat(in.nextLine());
+        //menyimpan data ke objek m1
+        System.out.print("Warna : ");
+        //meminta pengguna memasukan warna mobil
+        m1.setWarna(in.nextLine());
+        //menyimpan data ke objek m1
+        System.out.print("Kecepatan :");
+        //meminta pengguna memasukan kecepatan mobil
+        m1.setKecepatan(in.nextDouble());
+        //menyimpan data ke objek m1
+        System.out.print("Waktu dalam jam : ");
+        //meminta pengguna memasukan waktu 
+        m1.setWaktu(in.nextDouble());
+        //menyimpan data ke objek m1
+        m1.displayMessage();
+        //menampilkan data mobil
+        System.out.println("====================");
+        in.nextLine();
+        //untuk baris baru setelah input 
+
+        //instan objek baru bernama m2
+        Mobil m2 = new Mobil();
+        //membuat objek m2 dari kelas mobil
+
+        //Mulai baris 50-65 memiliki fungsi sama seperti baris 11-42 namun, pada baris 50-65 untuk mobil kedua (m2)
+        System.out.println("Masukan detail mobil 2 : ");
+        //meminta pengguna memasukan detail mobil
+        System.out.print("Manufaktur : ");
+        m2.setManufaktur(in.nextLine());
+        //menyimpan data ke objek m2
+        System.out.print("Nomor plat : ");
+        m2.setNoPlat(in.nextLine());
+        System.out.print("Warna : ");
+        m2.setWarna(in.nextLine());
+        System.out.print("Kecepatan : ");
+        m2.setKecepatan(in.nextDouble());
+        System.out.print ("Waktu dalam jam : ");
+        m2.setWaktu(in.nextDouble());
+        m2.displayMessage();
+        System.out.println("====================");
+        in.nextLine();
+
+        //merubah warna dari objek m1 menjadi warna hijau
+        System.out.println("mobil pada objek m1 di rubah menjadi hijau");
+        m1.setWarna("Hijau");
+
+        //menampilkan hasil/data perubahan warna mobl m1
+        m1.displayMessage();
+
+        //menutup scanner untuk mencegah kebocoran
+        in.close();
+    }
+
+}
---
+++ Mobil.java
@@ -0,0 +1,73 @@
+public class Mobil {
+    //membuat kelas mobil sebagai blueprint untuk objek mobil
+
+    //mendeklarasikan atribut 
+    private String noPlat; //untu nomor plat mobil
+    private String warna;//untuk warna mobil
+    private String manufaktur;//untuk menufaktur mobil
+    private double kecepatan;//untuk kecepatan mobil
+    private double waktu;//untuk waktu mobil
+    private double jarakdlmMeter;//untuk jarak mobil dalam satuan meter
+    private double jarak;//untuk jarak mobil dalam satuan Kilo meter
+
+    //mengubah nomor plat bedasarkan input pengguna
+    public void setNoPlat(String s){
+        noPlat = s;
+    }
+
+    //mengubah warna mobil sesuai input pengguna 
+    public void setWarna(String s){
+        warna = s;
+    }
+
+    //mengubah manufaktur mobil sesuai input pengguna 
+    public void setManufaktur(String a){
+        manufaktur = a;
+    }
+
+    //mengubah kecepatan mobil sesuai input pengguna 
+    public void setKecepatan(double i){
+        kecepatan = i;
+        rubahKecepatan();
+    }
+
+    //menbubah waktu tempuh mobil 
+    public void setWaktu(double w){
+        waktu = w;
+        rubahSekon(waktu);//memanggil rubahSekon(waktu)
+        hitungJarak();//memanggil hitung jarak, untuk mempermudah jika waktu berganti 
+    }
+
+    //mengubah waktu dari jam ke detik 
+    private void rubahSekon(double waktu){  //menggunakan private agar hanya dapat dipakai pada kelas Mobil
+        double sekon = waktu * 3600;  //menggunakan rumus waktu x 3600
+        System.out.println("Waktu dalam sekon = " + String.format("%.2f", sekon) + " s");  //menampilkan 2 angka dibelakang koma
+    }
+
+    //mengubah kecepatan dari Km/jam menjadi m/s
+    private void rubahKecepatan(){  //menggunakan private agar hanya dapat dipakai pada kelas Mobil
+        double kmpers = (kecepatan * 1000) / 3600;
+        System.out.println("Kecepatan dalam m/s = " + String.format("%.2f", kmpers) + " m/s");  //menampilkan 2 angka dibelakang koma
+
+    }
+
+    //menghitung jarak 
+    private void hitungJarak(){  //menggunakan private agar hanya dapat dipakai pada kelas Mobil
+        jarakdlmMeter = kecepatan * waktu *1000;  //menghitung jarak dalam satuan meter
+        System.out.println("Jarak = " + jarakdlmMeter + " m");  //menampilkan jarak dalam meter
+        jarak = jarakdlmMeter / 1000;  //menghitung jarak dalam satuan kilo meter 
+
+    }
+
+
+
+    //menampilkan semua informasi mobil
+    public void displayMessage(){
+        System.out.println("Mobil anda bermerek " + manufaktur);
+        System.out.println("mempunyai nomor plat " + noPlat);
+        System.out.println("serta memiliki warna " + warna);
+        System.out.println("dengan jarak " + jarak + " km");
+        System.out.println("dan mampu menempuh kecepatan " + kecepatan + " km/h");
+}
+
+} 
